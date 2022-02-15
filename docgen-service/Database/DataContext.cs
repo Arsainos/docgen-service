c4ae93ce1460419ac81b9bb6c206ca57f4a5c526
Белявцев Артём <Arsainos@gmail.com>
2022-02-15 17:03:11 +0300

    Зависимости и настройки ORM

---
+++ docgen-service/Database/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace docgen_service.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
