3bc56314d2fc4fcf9d9edc4740263f34add85adf
Белявцев Артём <Arsainos@gmail.com>
2022-02-20 15:16:27 +0300

    Add options controller

---
+++ docgen-service/Controllers/OptionsController.cs
@@ -0,0 +1,25 @@
+﻿using docgen_service.Configurations.AppSettings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace docgen_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OptionsController : ControllerBase
+    {
+        private readonly IOptions<Logging> _logging;
+        private readonly IOptions<DataBase> _database;
+        private readonly ILogger<OptionsController> _logger;
+
+        public OptionsController(ILogger<OptionsController> logger, IOptions<Logging> logging, IOptions<DataBase> database)
+        {
+            _logger = logger;
+            _logging = logging;
+            _database = database;
+        }
+
+
+    }
+}
